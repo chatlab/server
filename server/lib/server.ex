3d5c5ae52f0383681f43ff599dbd4abe4dc4e4f4
Leo Oliveira <qwerty124563@gmail.com>
2021-05-26 17:48:22 +0100

    Add Base files for server

---
+++ server/lib/server.ex
@@ -0,0 +1,19 @@
+defmodule Server do
+  @moduledoc """
+  Documentation for `Server`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Server.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
+
