3bdfbb95ad5838d6bab458e4ab7d865f62a5aeb8
Leo Oliveira <qwerty124563@gmail.com>
2021-06-03 19:34:55 +0100

    Add client example

---
+++ client.js
@@ -0,0 +1,15 @@
+const WebSocket = require('ws');
+
+ws = new WebSocket('ws://localhost:4333');
+ws.on('open', () => {
+
+    const message = {
+        event: 'join_room',
+        room_id: 'test123',
+    }
+    ws.send(message)
+
+    ws.on('message', message => {
+        console.log('Recived message: ', message);
+    });
+});
