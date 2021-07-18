631346bbb4d1a247cac77a237d0a0df92138d78f
Leo Oliveira <qwerty124563@gmail.com>
2021-07-18 01:41:47 +0100

    refactor(server): Add session_manager module

---
+++ server/lib/spot_server/session_manager.ex
@@ -0,0 +1,12 @@
+defmodule SpotServer.SessionManager do
+
+  def init() do
+    self()
+    |> inspect()
+    |> Base.encode32(case: :lower)
+    |> then(&"user_" <> &1)
+    |> String.to_atom()
+    |> then(&Process.register(self(), &1))
+  end
+
+end
--- server/lib/spot_server/websocket_controller.ex
+++ server/lib/spot_server/websocket_controller.ex
@@ -20,6 +20,7 @@ defmodule SpotServer.WebsocketController do
     {:ok, state}
   end
 
+
   def websocket_handle({:text, "ping"}, state) do
     Logger.info("Received a ping, responding a pong!")
     {:reply, {:text, "pong"}, state}
@@ -40,6 +41,20 @@ defmodule SpotServer.WebsocketController do
     Logger.info("Sending a ping")
     {:reply, {:ping, "server_ping"}, state}
   end
+
+  @impl :cowboy_websocket
+  def websocket_handle({:text, msg}, state) do
+    case Poison.decode(msg) do
+      {:ok, parsed_msg} ->
+        Logger.info("Parser message: " <> parsed_msg)
+        {:ok, Session.handle_message(parsed_msg, state)}
+
+      _ ->
+        answer = Poison.encode!(%{event: "error", description: "invalid json", received_msg: msg})
+        {:reply, {:text, answer}, state}
+    end
+  end
+
 
   @impl :cowboy_websocket
   def websocket_info(message, state) do
